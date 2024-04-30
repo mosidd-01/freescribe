947cde3ebbfe405ff9e59a2cb415d93a0a71e5ab
Muhammad Siddiqui <s760132@gmail.com>
2024-04-30 14:41:08 -0500

    Added voice recording capabilities

--- free-scribe/src/components/FileDisplay.jsx
+++ free-scribe/src/components/FileDisplay.jsx
@@ -1,13 +1,13 @@
 export default function FileDisplay(props) {
   const { file, audioStream, handleAudioRest } = props;
   return (
-    <main className="flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5 justify-center text-center pb-20 w-fit max-w-full mx-auto">
+    <main className="flex-1 p-4 flex flex-col gap-3 sm:gap-4 justify-center text-center pb-20 w-72 sm:w-96 max-w-full mx-auto">
       <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
         Your <span className="text-blue-400 bold">File</span>
       </h1>
-      <div className="mx-auto flex flex-col text-left my-4">
+      <div className="flex flex-col text-left my-4">
         <h3 className="font-semibold">Name</h3>
-        <p>{file.name}</p>
+        <p className="truncate">{file ? file?.name : "Custom audio"}</p>
         <div className="flex items-center justify-between gap-4 mt-5">
           <button
             onClick={handleAudioRest}
