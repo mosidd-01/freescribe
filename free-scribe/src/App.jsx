4c612abfaf3520f4b7359426d425b0f88c80c59e
Muhammad Siddiqui <s760132@gmail.com>
2024-04-29 22:45:32 -0500

    added fonts and icon packages

--- free-scribe/src/App.jsx
+++ free-scribe/src/App.jsx
@@ -3,7 +3,11 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-green-400">Hello</h1>
+    </div>
+  );
 }
 
 export default App;
