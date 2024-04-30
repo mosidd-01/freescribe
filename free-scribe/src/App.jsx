5cc6e573f1cf1cd83a9e36f8aeae3df803db82b1
Muhammad Siddiqui <s760132@gmail.com>
2024-04-30 00:39:31 -0500

    Created frontend page and upload page

--- free-scribe/src/App.jsx
+++ free-scribe/src/App.jsx
@@ -1,11 +1,35 @@
 import { useState } from "react";
+import HomePage from "./components/HomePage";
+import Header from "./components/Header";
+import FileDisplay from "./components/FileDisplay";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [file, setFile] = useState(null);
+  const [audioStream, setAudioStream] = useState(null);
+
+  const isAudioAvailible = file || audioStream;
+
+  function handleAudioRest() {
+    setFile(null);
+    setAudioStream(null);
+  }
 
   return (
-    <div>
-      <h1 className="text-green-400">Hello</h1>
+    <div className="flex flex-col max-w-[1000px] mx-auto w-full">
+      <section className="min-h-screen flex flex-col">
+        <Header />
+        {isAudioAvailible ? (
+          <FileDisplay
+            file={file}
+            audioStream={audioStream}
+            handleAudioRest={handleAudioRest}
+          />
+        ) : (
+          <HomePage setFile={setFile} setAudioStream={setAudioStream} />
+        )}
+      </section>
+      <h1 className="text-green-400">hello</h1>
+      <footer></footer>
     </div>
   );
 }
---
+++ free-scribe/src/components/FileDisplay.jsx
@@ -0,0 +1,26 @@
+export default function FileDisplay(props) {
+  const { file, audioStream, handleAudioRest } = props;
+  return (
+    <main className="flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5 justify-center text-center pb-20 w-fit max-w-full mx-auto">
+      <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
+        Your <span className="text-blue-400 bold">File</span>
+      </h1>
+      <div className="mx-auto flex flex-col text-left my-4">
+        <h3 className="font-semibold">Name</h3>
+        <p>{file.name}</p>
+        <div className="flex items-center justify-between gap-4 mt-5">
+          <button
+            onClick={handleAudioRest}
+            className="text-slate-400 hover:text-blue-600 duration-200"
+          >
+            Reset
+          </button>
+          <button className="specialBtn px-3 py-2 rounded-lg text-blue-400 flex items-center gap-2 font-medium">
+            <p>Transcribe</p>
+            <i className="fa-solid fa-pencil"></i>
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
